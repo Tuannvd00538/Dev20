4cf5e20ffa73459179a4e2ab000c1e77dae4ae44
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-06-08 12:17:04 +0700

    change log

--- controller/warning.js
+++ controller/warning.js
@@ -10,90 +10,81 @@ exports.getWarningByUser = async (req, res) => {
     var today = new Date();
     var mm = String(today.getMonth() + 1).padStart(2, '');
     var yyyy = today.getFullYear();
+
+    var dataResult = {
+        code: 200,
+        today: [],
+        month: [],
+        year: []
+    }
+
+    // query by year
+    await Warning.aggregate([
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, temperature: 1, isWarning: 1, ownerId: 1, year: { $year: '$createdAt' } } },
+        {
+            $match: {
+                year: yyyy,
+                ownerId: mongoose.Types.ObjectId(id),
+                isWarning: true
+            }
+        }
+    ], (err, result) => {
+        if (err) return;
+        if (result.length != 0) {
+            dataResult.year = result;
+            return;
+        };
+        dataResult.year = [{
+            message: "No data!"
+        }];
+    }).exec();
     const todayGet = moment().startOf('day');
 
-    try {
-        let nam = Warning.aggregate([
-            { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, temperature: 1, isWarning: 1, ownerId: 1, year: { $year: '$createdAt' } } },
-            {
-                $match: {
-                    year: yyyy,
-                    ownerId: mongoose.Types.ObjectId(id),
-                    isWarning: true
-                }
+    // query by date
+    await Warning.aggregate([
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, isWarning: 1, temperature: 1, ownerId: 1 } },
+        {
+            $match: {
+                createdAt: { $gte: todayGet.toDate(), $lt: moment(todayGet).endOf('day').toDate() },
+                ownerId: mongoose.Types.ObjectId(id)
             }
-        ], (err, result) => {
-            if (err) return;
-            if (result.length != 0) {
-                dataResult.year = result;
-                return;
-            };
-            dataResult.year = [{
-                message: "No data!"
-            }];
-        }).exec();
-
-        let thang = Warning.aggregate([
-            { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, month: { $month: '$createdAt' }, isWarning: 1, temperature: 1, ownerId: 1, year: { $year: '$createdAt' } } },
-            {
-                $match: {
-                    year: parseInt(yyyy),
-                    month: parseInt(mm),
-                    ownerId: mongoose.Types.ObjectId(id),
-                    isWarning: true
-                }
-            }
-        ], (err, result) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            if (result.length != 0) {
-                dataResult.month = result;
-                return;
-            };
-            dataResult.month = [{
-                message: "No data!"
-            }];
-        }).exec();
-
-        let ngay = Warning.aggregate([
-            { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, isWarning: 1, temperature: 1, ownerId: 1 } },
-            {
-                $match: {
-                    createdAt: { $gte: todayGet.toDate(), $lt: moment(todayGet).endOf('day').toDate() },
-                    ownerId: mongoose.Types.ObjectId(id)
-                }
-            }
-        ], (err, result) => {
-            if (err) return;
-            if (result.length != 0) {
-                dataResult.today = result;
-                return;
-            };
-            dataResult.today = [{
-                message: "No data!"
-            }];
-        }).exec();
-
-        let dataResult = {
-            code: 200,
-            today: ngay,
-            month: thang,
-            year: nam
         }
+    ], (err, result) => {
+        if (err) return;
+        if (result.length != 0) {
+            dataResult.today = result;
+            return;
+        };
+        dataResult.today = [{
+            message: "No data!"
+        }];
+    }).exec();
 
-        let result = await Promise.all([nam, thang, ngay]);
-        
-        res.status(200).json(result);
-        
-
-    } catch (error) {
-
-        console.log(error);
-        
-
-    }
+    // query by month
+    await Warning.aggregate([
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, month: { $month: '$createdAt' }, isWarning: 1, temperature: 1, ownerId: 1, year: { $year: '$createdAt' } } },
+        {
+            $match: {
+                year: parseInt(yyyy),
+                month: parseInt(mm),
+                ownerId: mongoose.Types.ObjectId(id),
+                isWarning: true
+            }
+        }
+    ], (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if (result.length != 0) {
+            dataResult.month = result;
+            return;
+        };
+        dataResult.month = [{
+            message: "No data!"
+        }];
+    }).exec();
+    res.status(200).json(dataResult);
 }
 
 exports.add = function (req, res) {
@@ -118,7 +109,7 @@ exports.getByMonthAndYear = function (req, res) {
     const qrMonth = req.params.month;
     const qrYear = req.params.year;
     Warning.aggregate([
-        { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, month: { $month: '$createdAt' }, isWarning: 1, temperature: 1, ownerId: 1, year: { $year: '$createdAt' } } },
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, month: { $month: '$createdAt' }, isWarning: 1, temperature: 1, ownerId: 1, year: { $year: '$createdAt' } } },
         {
             $match: {
                 month: parseInt(qrMonth),
@@ -153,7 +144,7 @@ exports.getByYear = function (req, res) {
     const id = req.params.id;
     const qrYear = req.params.year;
     Warning.aggregate([
-        { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, temperature: 1, isWarning: 1, ownerId: 1, year: { $year: '$createdAt' } } },
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, temperature: 1, isWarning: 1, ownerId: 1, year: { $year: '$createdAt' } } },
         {
             $match: {
                 year: parseInt(qrYear),
@@ -189,7 +180,7 @@ exports.getToday = function (req, res) {
     const today = moment().startOf('day');
 
     Warning.aggregate([
-        { $project: { createdAt: { $subtract: ["$createdAt", new Date("1970-01-01")] }, isWarning: 1, temperature: 1, ownerId: 1 } },
+        { $project: { createdAt: { $subtract: [ "$createdAt", new Date("1970-01-01") ] }, isWarning: 1, temperature: 1, ownerId: 1 } },
         {
             $match: {
                 createdAt: { $gte: today.toDate(), $lt: moment(today).endOf('day').toDate() },
